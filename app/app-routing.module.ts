538a8d4b62ff81e6e1b754a070b7bd3cfaef51ae
Tanmoy Sen <28593@ITCINFOTECH.COM>
2019-12-09 14:04:48 +0530

    tanmoy added

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { FlyClubMainComponent } from './fly-club-main/fly-club-main.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MyProfileComponent } from './flyClubMain/my-profile/my-profile.component';
+import { MyTripsComponent } from './flyClubMain/my-trips/my-trips.component';
+
 const routes: Routes = [{path: '', redirectTo: 'FlyClub', pathMatch: 'full'},
-{path: 'FlyClub', component: FlyClubMainComponent, children:[{path: 'myprofile', component: MyProfileComponent}]},
+{path: 'FlyClub', component: FlyClubMainComponent, children:[{path: 'myprofile', component: MyProfileComponent}, {path: 'mytrips', component: MyTripsComponent}]},
 {path: 'page-not-found', component: PageNotFoundComponent},
 {path: '**', redirectTo: '/page-not-found', pathMatch: 'prefix'}
 ];
--- app/app.module.ts
+++ app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FlyClubMainComponent } from './fly-club-main/fly-club-main.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MyProfileComponent } from './flyClubMain/my-profile/my-profile.component';
+import { MyTripsComponent } from './flyClubMain/my-trips/my-trips.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FlyClubMainComponent,
     PageNotFoundComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    MyTripsComponent
   ],
   imports: [
     BrowserModule,
